315733c960e67c45cb2da143c3526f298544eb2d
ramillasairaj <166886557+ramillasairaj@users.noreply.github.com>
2024-09-23 18:49:53 +0530

    Update Student_Grade_Calculator.java

--- Student_Grade_Calculator.java
+++ Student_Grade_Calculator.java
@@ -1,39 +1,18 @@
 import java.util.Scanner;
 
-public class GradeCalculator{
+public class GradeCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter the number of subjects: ");
         int numSubjects = scanner.nextInt();
-        scanner.nextLine(); // Consume the newline character
 
-        String[] subjectNames = new String[numSubjects];
-        int[] marksObtained = new int[numSubjects];
         int totalMarks = 0;
-        int highestMarks = Integer.MIN_VALUE;
-        int lowestMarks = Integer.MAX_VALUE;
 
-        for (int i = 0; i < numSubjects; i++) {
-            System.out.print("Enter the name of subject " + (i + 1) + ": ");
-            subjectNames[i] = scanner.nextLine();
-
-            int marks;
-            do {
-                System.out.print("Enter marks obtained in " + subjectNames[i] + " (0-100): ");
-                marks = scanner.nextInt();
-            } while (marks < 0 || marks > 100); // Input validation
-            marksObtained[i] = marks;
-
+        for (int i = 1; i <= numSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + i + ": ");
+            int marks = scanner.nextInt();
             totalMarks += marks;
-
-            // Update highest and lowest marks
-            if (marks > highestMarks) {
-                highestMarks = marks;
-            }
-            if (marks < lowestMarks) {
-                lowestMarks = marks;
-            }
         }
 
         double averagePercentage = (double) totalMarks / (numSubjects * 100) * 100;
@@ -51,18 +30,10 @@ public class GradeCalculator{
             grade = 'F';
         }
 
-        // Display results
-        System.out.println("----------------------------");
-        System.out.println("Subject-wise Marks:");
-        for (int i = 0; i < numSubjects; i++) {
-            System.out.println(subjectNames[i] + ": " + marksObtained[i]);
-        }
-        System.out.println("----------------------------");
+       
         System.out.println("Total Marks: " + totalMarks);
         System.out.println("Average Percentage: " + averagePercentage + "%");
         System.out.println("Grade: " + grade);
-        System.out.println("Highest Marks: " + highestMarks);
-        System.out.println("Lowest Marks: " + lowestMarks);
 
         scanner.close();
     }
